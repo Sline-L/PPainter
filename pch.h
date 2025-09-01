58c4d53dcc9ab48e09acc76d09741101d1b71411
Sline-L <glx15424872@gmail.com>
2025-09-01 11:31:40 +0800

    edit CMakeLists.txt

---
+++ pch.h
@@ -0,0 +1,89 @@
+// pch.h - 预编译头文件
+#ifndef PCH_H
+#define PCH_H
+
+// Qt核心模块
+#include <QtCore>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QList>
+#include <QVector>
+#include <QMap>
+#include <QHash>
+#include <QQueue>
+#include <QStack>
+#include <QTimer>
+#include <QThread>
+#include <QMutex>
+#include <QSettings>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+// Qt GUI模块
+#include <QtGui>
+#include <QApplication>
+#include <QWidget>
+#include <QMainWindow>
+#include <QPainter>
+#include <QImage>
+#include <QPixmap>
+#include <QColor>
+#include <QBrush>
+#include <QPen>
+#include <QCursor>
+#include <QFont>
+#include <QFontMetrics>
+
+// Qt Widgets模块
+#include <QtWidgets>
+#include <QToolBar>
+#include <QMenuBar>
+#include <QStatusBar>
+#include <QDockWidget>
+#include <QScrollArea>
+#include <QSplitter>
+#include <QLayout>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QListWidget>
+#include <QTreeWidget>
+#include <QTableWidget>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QSlider>
+#include <QProgressBar>
+#include <QGroupBox>
+#include <QCheckBox>
+#include <QRadioButton>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QColorDialog>
+
+// Qt Network模块
+#include <QtNetwork>
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+// C++标准库
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+#endif // PCH_H
